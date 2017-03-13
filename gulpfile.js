5746498d8ef278be7e0f9c086fa2322d983075fb
Christian Cros <christian@christiancros.com>
2017-03-13 06:05:26 -0700

    Refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,45 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var concat = require('gulp-concat');
+/**
+	Namespace definitions.
 
+	- Transfer: Include a dependency from node_modules into the public dir.
+	- Build: Concatenate and minify dependencies.
+	- Watch: Observe changes and execute specific tasks accordingly.
+ */
 
-var paths = {
-	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+const concat = require('gulp-concat');
 
+const paths = {
+	css: {
+		normalize: 'node_modules/normalize.css/normalize.css',
+		main: 'public/styles/main.css',
+	},
+
+	js: {
+		jquery: 'node_modules/jquery/dist/jquery.js',
+	},
 };
 
-gulp.task('bundleStyles', function () {
-	return gulp.src(['node_modules/normalize.css/normalize.css', 'public/styles/main.css'])
-		.pipe(concat('main.min.css'))
+
+// Transfers
+gulp.task('transfer:normalize', function () {
+	return gulp.src(paths.css.normalize)
 		.pipe(gulp.dest('./public/styles'));
 });
 
-gulp.task('bundle:vendorjs', function () {
-	return gulp.src(['node_modules/jquery/dist/jquery.js'])
+
+// Bundlers
+gulp.task('bundle:js', function () {
+	return gulp.src(paths.js)
 		.pipe(concat('vendor.js'))
 		.pipe(gulp.dest('public/js'));
 });
 
+
+// Watchers
 gulp.task('watch:css', function () {
-	gulp.watch(['public/styles/main.css'], ['bundleStyles']);
+	gulp.watch(['public/styles/main.css'], ['bundle:css']);
 });
 
 gulp.task('default', ['watch:css']);
