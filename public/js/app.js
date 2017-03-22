b068c2e3554603802457d7888357da3ddfae2edf
Christian Cros <christian@christiancros.com>
2017-03-21 17:37:12 -0700

    Fixed dependency bug, made config to load first

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,6 @@
-require(['config', 'app/navigation', 'app/top-selector', 'app/soundcloud'], function () {
-	console.log('All dependencies have been loaded');
+// Load the config first
+require(['config'], function () {
+	require(['app/navigation', 'app/top-selector', 'app/soundcloud'], function () {
+		console.log('All dependencies have been loaded');
+	});
 });
